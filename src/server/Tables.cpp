68d98b44c6edc72a762ebcfa167b39d597f07003
Cristi D <cristi.dnla@gmail.com>
2015-05-20 22:53:15 +0300

    Implemented Tables class

--- src/server/Tables.cpp
+++ src/server/Tables.cpp
@@ -8,10 +8,31 @@ Tables::Tables()
 
 }
 
+void Tables::AddTable(WhistGame::Table* table)
+{
+    tables.push_back(table);
+}
+
+void Tables::RemoveTable(WhistGame::Table* table)
+{
+    tables.remove(table);
+}
+
+WhistGame::Table* Tables::GetTable(unsigned int id)
+{
+    int i = 0;
+    for(auto it = tables.begin(); it != tables.end(); it++, i++)
+    {
+        if(i == id)
+            return *it;
+    }
+
+    return NULL;
+}
+
 Tables::~Tables()
 {
 
 }
 
 }
-
