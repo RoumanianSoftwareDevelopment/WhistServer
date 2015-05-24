72f607923cbbfa305bdbc14ff97f31e6ae4b38b1
EnachescuAlin <alinenachescu033@gmail.com>
2015-05-24 09:44:19 +0300

    Added the function declaration IsThereAnyCommand().

--- src/server/Connection.h
+++ src/server/Connection.h
@@ -10,24 +10,25 @@ namespace WhistGame {
 class Connection : public std::enable_shared_from_this<Connection>
 {
     public:
-        Connection(boost::asio::ip::tcp::socket sock,
-                   WhistGame::Players* _players);
+        Connection(boost::asio::ip::tcp::socket, Players*, Tables*);
         virtual~ Connection();
         void Start();
     private:
         void Read();
-        void Write(std::size_t length);
+        void Write(std::size_t);
+        bool IsThereAnyCommand(char*);
 
         boost::asio::ip::tcp::socket playerSocket;
         HandlerAllocator allocator;
         std::string writeBuffer;
         Players *players;
         Player player;
+        Tables *tables;
         ProcessingCommand processingCommand;
         char *readBuffer;
 };
 
-};
+}
 
 #endif
 
